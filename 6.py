fa696353e99c661b57e1b701859f89d329489cb2
доходяга <113695478+nnnhurt@users.noreply.github.com>
2023-03-31 15:15:59 +0300

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,16 +1,15 @@
-def symmetric(a):
+def symmetric(a, n):
     for i in range(n):
         for j in range(n):
             if a[i][j] != a[j][i]:
-                return False #если находится неравенство, то сразу же возвращаем фолс
+                return False ##если находится неравенство в противополож оси, то сразу же возвращаем фолс
     return True
 
 
 n = int(input())
 b = [list(map(int, input().split())) for i in range(n)]
-if symmetric(b):
+if symmetric(b,n):
     print('YES')
 else:
     print('NO')
-
 
